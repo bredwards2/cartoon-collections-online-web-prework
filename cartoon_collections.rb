0575a6a74f1b590439b39febe2fbe478d522b6a9
Britny Edwards <bredwards2@gmail.com>
2018-11-25 03:52:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves# code an argument here
   def roll_call_dwarves
     puts dwarves = ["Dopey", "Grumpy", "Bashful"]
     dwarves.each_with_index
-end
+   end
 
 def summon_captain_planet# code an argument here
   # Your code here
@@ -13,16 +13,16 @@ end
 
   fruits = ["apple", "banana", "orange"]
   fruits.captilize << !
-end
+  end
 
 def long_planeteer_calls# code an argument here
   # Your code here
   calls_long = ["axe", "earth", "wind", "fire", "water", "heart"]
   calls_long.any? do |word|
   word.length == 4
-  end
+end
   calls_long.any? do |word|
-    word.length > 4
+  word.length > 4
   end
 
 def find_the_cheese# code an argument here
