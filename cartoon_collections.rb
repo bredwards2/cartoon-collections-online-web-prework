bb459b521c47245010c0e99409fbf693ac4dfcaf
Britny Edwards <bredwards2@gmail.com>
2018-11-25 03:49:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ def long_planeteer_calls# code an argument here
   # Your code here
   calls_long = ["axe", "earth", "wind", "fire", "water", "heart"]
   calls_long.any? do |word|
-    
+    word.length == 4
+  end
 
 end
 
