1efc9461330ef92d8a89615df5335a4e0766294d
Britny Edwards <bredwards2@gmail.com>
2018-11-25 03:54:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,33 +1,25 @@
-def roll_call_dwarves# code an argument here
-
-  def roll_call_dwarves
-    puts dwarves = ["Dopey", "Grumpy", "Bashful"]
-    dwarves.each_with_index
-   end
-
-def summon_captain_planet# code an argument here
-  # Your code here
-  veggies = ["carrot", "cucumber", "pepper"]
-  veggies.length
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-  fruits = ["apple", "banana", "orange"]
-  fruits.captilize << !
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
   end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-  calls_long = ["axe", "earth", "wind", "fire", "water", "heart"]
-  calls_long.any? do |word|
-  word.length == 4
+  planeteer_calls
 end
-  calls_long.any? do |word|
-  word.length > 4
+
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end
+end
+
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
+end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.index[0]
-end
-end
