b4ea4da8ac5844695c38e003d2faf7c59028590c
Britny Edwards <bredwards2@gmail.com>
2018-11-25 03:39:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
+
+  def roll_call_dwarves
+    puts dwarves = ["Dopey", "Grumpy", "Bashful"]
+    
+
 end
 
 def summon_captain_planet# code an argument here
